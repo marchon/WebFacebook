0c8c5fb72606b1fbb7e5b3494813c503aa51f11c
streambinder <davidepucci@hiddenhost.org>
2015-01-27 20:11:04 +0100

    webfacebook: add splashscreen

--- src/com/hiddenhost/webfacebook/Index.java
+++ src/com/hiddenhost/webfacebook/Index.java
@@ -7,39 +7,47 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class Index extends Activity {
 
+	private Handler mHandler = new Handler();
     private WebView mWebView;
     private String url = "https://m.facebook.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (!isNetworkAvailable()) {
-        	setContentView(R.layout.activity_offline);
-        } else {
-        	requestWindowFeature(Window.FEATURE_NO_TITLE);
-        	setContentView(R.layout.activity_index);
-        	mWebView = (WebView) findViewById(R.id.webview);
-        	mWebView.getSettings().setJavaScriptEnabled(true);
-        	mWebView.getSettings().setLoadsImagesAutomatically(true);
-        	mWebView.setWebViewClient(new WebViewClient() {
-        		public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        			if (url != null && (url.startsWith("http://") || url.startsWith("https://"))) {
-        				view.getContext().startActivity(
-        				new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
-        				return true;
-        			} else {
-        				return false;
-        			}
-        		}
-        	});
-        	mWebView.loadUrl(url);
-        }
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+    	setContentView(R.layout.activity_splashscreen);
+    	mHandler.postDelayed(new Runnable() {
+            public void run() {
+            	if (!isNetworkAvailable()) {
+                	setContentView(R.layout.activity_offline);
+                } else {
+                	requestWindowFeature(Window.FEATURE_NO_TITLE);
+                	setContentView(R.layout.activity_index);
+                	mWebView = (WebView) findViewById(R.id.webview);
+                	mWebView.getSettings().setJavaScriptEnabled(true);
+                	mWebView.getSettings().setLoadsImagesAutomatically(true);
+                	mWebView.setWebViewClient(new WebViewClient() {
+                		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                			if (url != null && (url.startsWith("http://") || url.startsWith("https://"))) {
+                				view.getContext().startActivity(
+                				new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                				return true;
+                			} else {
+                				return false;
+                			}
+                		}
+                	});
+                	mWebView.loadUrl(url);
+                }
+            }
+        }, 3000);
     }
 
     private boolean isNetworkAvailable() {
