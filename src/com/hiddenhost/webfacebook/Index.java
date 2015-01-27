b5aec4af33e498df05f54d8c7ad5849256088673
streambinder <davidepucci@hiddenhost.org>
2015-01-27 19:20:36 +0100

    webfacebook: open URIs with default browser app

--- src/com/hiddenhost/webfacebook/Index.java
+++ src/com/hiddenhost/webfacebook/Index.java
@@ -1,6 +1,8 @@
 package com.hiddenhost.webfacebook;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Window;
 import android.webkit.WebView;
@@ -19,9 +21,18 @@ public class Index extends Activity {
         mWebView = (WebView) findViewById(R.id.webview);
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.getSettings().setLoadsImagesAutomatically(true);
-        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url != null && (url.startsWith("http://") || url.startsWith("https://"))) {
+                    view.getContext().startActivity(
+                        new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
         mWebView.loadUrl(url);
     }
-
 
 }
