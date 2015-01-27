75690684fb8d23f2dd0e26871725d6cc895d5bb3
streambinder <davidepucci@hiddenhost.org>
2015-01-27 20:00:17 +0100

    webfacebook: enable network state check at start

--- src/com/hiddenhost/webfacebook/Index.java
+++ src/com/hiddenhost/webfacebook/Index.java
@@ -1,7 +1,10 @@
 package com.hiddenhost.webfacebook;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Window;
@@ -16,23 +19,34 @@ public class Index extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_index);
-        mWebView = (WebView) findViewById(R.id.webview);
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.getSettings().setLoadsImagesAutomatically(true);
-        mWebView.setWebViewClient(new WebViewClient() {
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if (url != null && (url.startsWith("http://") || url.startsWith("https://"))) {
-                    view.getContext().startActivity(
-                        new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        });
-        mWebView.loadUrl(url);
+        if (!isNetworkAvailable()) {
+        	setContentView(R.layout.activity_offline);
+        } else {
+        	requestWindowFeature(Window.FEATURE_NO_TITLE);
+        	setContentView(R.layout.activity_index);
+        	mWebView = (WebView) findViewById(R.id.webview);
+        	mWebView.getSettings().setJavaScriptEnabled(true);
+        	mWebView.getSettings().setLoadsImagesAutomatically(true);
+        	mWebView.setWebViewClient(new WebViewClient() {
+        		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        			if (url != null && (url.startsWith("http://") || url.startsWith("https://"))) {
+        				view.getContext().startActivity(
+        				new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+        				return true;
+        			} else {
+        				return false;
+        			}
+        		}
+        	});
+        	mWebView.loadUrl(url);
+        }
     }
+
+    private boolean isNetworkAvailable() {
+	    ConnectivityManager connectivityManager 
+	          = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+	    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+	    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+	}
 
 }
