d4caa9b7dac7fc3cc0d44d38419ef56eb8398181
streambinder <davidepucci@hiddenhost.org>
2015-01-28 11:45:16 +0100

    webfacebook: splash until WebView correctly load page

--- src/com/hiddenhost/webfacebook/Index.java
+++ src/com/hiddenhost/webfacebook/Index.java
@@ -7,46 +7,48 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
 import android.view.Window;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
 
 public class Index extends Activity {
 
-	private Handler mHandler = new Handler();
-    private WebView mWebView;
-    private String url = "https://m.facebook.com/";
+	private ImageView SplashImage;
+	private String url = "https://m.facebook.com/";
+	private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-    	setContentView(R.layout.activity_splashscreen);
-    	mHandler.postDelayed(new Runnable() {
-            public void run() {
-            	if (!isNetworkAvailable()) {
-                	setContentView(R.layout.activity_offline);
-                } else {
-                	setContentView(R.layout.activity_index);
-                	mWebView = (WebView) findViewById(R.id.webview);
-                	mWebView.getSettings().setJavaScriptEnabled(true);
-                	mWebView.getSettings().setLoadsImagesAutomatically(true);
-                	mWebView.setWebViewClient(new WebViewClient() {
-                		public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                			if (url != null && !url.startsWith("https://m.facebook")) {
-                				view.getContext().startActivity(
-                				new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
-                				return true;
-                			} else {
-                				return false;
-                			}
-                		}
-                	});
-                	mWebView.loadUrl(url);
-                }
-            }
-        }, 3000);
+    	setContentView(R.layout.activity_index);
+    	if (!isNetworkAvailable()) {
+    		setContentView(R.layout.activity_offline);
+        } else {
+        	setContentView(R.layout.activity_index);
+        	SplashImage = (ImageView) findViewById(R.id.splash_image);
+        	mWebView = (WebView) findViewById(R.id.webview);
+        	mWebView.getSettings().setJavaScriptEnabled(true);
+        	mWebView.getSettings().setLoadsImagesAutomatically(true);
+        	mWebView.setWebViewClient(new WebViewClient() {
+        		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        			if (url != null && !url.startsWith("https://m.facebook")) {
+        				view.getContext().startActivity(
+        				new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+        				return true;
+        			} else {
+        				return false;
+        			}
+        		}
+        		public void onPageFinished(WebView view, String url) {
+        			mWebView.setVisibility(View.VISIBLE);
+        			SplashImage.setVisibility(View.GONE);
+        		}
+  			});
+        	mWebView.loadUrl(url);
+        }
     }
 
     private boolean isNetworkAvailable() {
